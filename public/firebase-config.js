7ce345caf5f5c802b1e1ed7bf8a3a0cd980c7a9e
あなたの名前 <kojima.mizuki@outlook.com>
2025-10-12 22:39:02 +0900

    chore(firebase): add firebase-config export for web app

---
+++ public/firebase-config.js
@@ -0,0 +1,10 @@
+// public/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDlpw9zV4coV6QoWNvQjQNuRJZyCamPOwI",
+  authDomain: "missioncompass-3b58e.firebaseapp.com",
+  projectId: "missioncompass-3b58e",
+  storageBucket: "missioncompass-3b58e.firebasestorage.app",
+  messagingSenderId: "131331323629",
+  appId: "1:131331323629:web:d3569e87cfc769b739fee4",
+  measurementId: "G-6QPMD953N7"
+};
