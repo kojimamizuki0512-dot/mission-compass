af7fa96673128cdc51c86eb813b5bc51502b8cdf
あなたの名前 <kojima.mizuki@outlook.com>
2025-10-13 23:09:43 +0900

    Switch to Firebase compat (global) + onclick handlers; fix login click & enable send button with IME handling

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,9 +1,5 @@
-// Firebase Web v9+ (Modular)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
-
-// ↓↓↓ コンソール > プロジェクトの設定 > マイアプリ > Webアプリ の Config を丸ごと貼る ↓↓↓
-const firebaseConfig = {
+// public/firebase-config.js
+window.__FIREBASE_CONFIG__ = {
   apiKey: "AIzaSyDlpw9zV4coV6QoWNvQjQNuRJZyCamPOwI",
   authDomain: "missioncompass-3b58e.firebaseapp.com",
   projectId: "missioncompass-3b58e",
@@ -12,19 +8,3 @@ const firebaseConfig = {
   appId: "1:131331323629:web:d3569e87cfc769b739fee4",
   measurementId: "G-6QPMD953N7"
 };
-// ↑↑↑ ここまで ↑↑↑
-
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
-
-// 使い方例：importして dialog.html のスクリプトで signIn/signOut を呼ぶ
-export async function signInWithGoogle() {
-  await signInWithPopup(auth, provider);
-}
-export async function signOutAll() {
-  await signOut(auth);
-}
-export function onAuth(cb) {
-  return onAuthStateChanged(auth, cb);
-}
